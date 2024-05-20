e0c080420439f11b835ab58e1ff76e522d756fa2
류솔아 <mav7937@naver.com>
2024-05-20 11:40:55 +0900

    TAB 배열을 활용한  배경이미지 변경

--- 03-TAB/01-기본/js/script.js
+++ 03-TAB/01-기본/js/script.js
@@ -5,14 +5,9 @@ $(function () {
 
   console.log(tabMenu, tabContent);
 
-  // 초기 세팅
-  // 첫번째 탭메뉴가 활성화되게끔
-  tabMenu.eq(0).addClass('on');
-
-  // 전체 콘텐츠 숨기기
-  // 첫번째 항목의 콘텐츠만 보이게
-  tabContent.hide();
-  tabContent.eq(0).show();
+  // 초기 세팅 - 함수의 실행
+  // 공통 동작이 2번 일어나기 때문에 tabAction이라는 함수로 정의
+  tabAction(0);
 
   // 탭메뉴를 클릭했을 때,
   tabMenu.on('click', function (e) {
@@ -22,16 +17,24 @@ $(function () {
     const tabIdx = $(this).index();
     console.log(tabIdx);
 
+    // 함수의 실행
+    // tabLdx의 값이 tabAction의 개구멍(index)를 통해서 input되고, output으로 return 됨
+    tabAction(tabIdx);
+  });
+
+  // 공통의 동작을 함수로 정의
+  // 여기에서 index는 변수의 이름이기 때문에 어떤 이름이어도 상관이 없음
+  function tabAction(index) {
+    // 탭메뉴 활성화
+    tabMenu.removeClass('on');
+    // 탭 메뉴 활성화되기 전에, on 클래스가 들어간 모든 tabMenu에서 on 클래스 삭제
+    // 클릭한 탭 메뉴에게만 on 클래스 부여
+    tabMenu.eq(index).addClass('on');
+
     // 탭 콘텐츠 중에서 인덱스에 해당하는 내용을 보여지게
     // 먼저 다 숨기고
     tabContent.hide();
     // 활성화된 탭 콘텐츠를 보여주기
-    tabContent.eq(tabIdx).show();
-
-    // 탭메뉴 활성화
-    // 탭 메뉴 활성화되기 전에, on 클래스가 들어간 모든 tabMenu에서 on 클래스 삭제
-    // 클릭한 탭 메뉴에게만 on 클래스 부여
-    tabMenu.removeClass('on');
-    tabMenu.eq(tabIdx).addClass('on');
-  });
+    tabContent.eq(index).show();
+  }
 });
