1fdbe1169af744fe4d065fe8c92e4d81f2b0c670
류솔아 <mav7937@naver.com>
2024-05-22 10:49:28 +0900

    동영상 자동 재생 및 창닫을 시 동영상 멈춤

--- 04-popup/03-유튜브 동영상 갤러리/01-기본/js/script.js
+++ 04-popup/03-유튜브 동영상 갤러리/01-기본/js/script.js
@@ -9,8 +9,11 @@ $(function () {
   // 비디오 리스트를 클릭(선택)했을 때
   $selectVideo.on('click', function () {
     // 선택한 비디오 링크를 받아서 변수에 저장
-    const videoLink = $(this).data('link'); /* data-link를 가지고 옴 */
+    let videoLink = $(this).data('link'); /* data-link를 가지고 옴 */
     console.log(videoLink);
+
+    // videoLink = videoLink + '?autoplay=1'
+    videoLink += '?autoplay=1';
     // $video의 src값으로 비디오 링크를 저장
     $video.attr('src', videoLink);
 
@@ -22,12 +25,16 @@ $(function () {
     // 선택한 비디오의 텍스트를 변수에 저장
     const $videoText = $(this).text();
     // .caption에 세팅
-    // 클릭하면 선택한 li의 텍스트를 가져오고 대체함
     $('.caption').text($videoText);
   });
 
   $btnClose.add($dim).on('click', function () {
+    // dim 사라지게
     $dim.fadeOut();
+    // videowrap을 보이지 않게
     $videoWrap.removeClass('active');
+
+    // $video의 src 값을 없애자 --> 동영상 삭제 / 소리가 들리지 않게 하기 위함
+    $video.attr('src', '');
   });
 });
